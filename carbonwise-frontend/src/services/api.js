55281f0b061467b2022edce089aaeeda02a469a7
Jaswant-Karun <jkstech37@gmail.com>
2026-03-17 21:23:06 +0530

    Fix API URL

--- carbonwise-frontend/src/services/api.js
+++ carbonwise-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://carbonwise-ai.onrender.com/api",
 });
 
 // Add a request interceptor to inject the token
